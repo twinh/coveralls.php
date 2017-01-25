84c2d8b83e77bea1c945173afbd2339007a90068
Cédric Belin <cedric@belin.io>
2017-01-25 20:00:01 +0100

    Added the first unit tests of the `Configuration` class

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -36,46 +36,46 @@ class Configuration implements \ArrayAccess, \Countable, \IteratorAggregate, \Js
 
   /**
    * Creates a new configuration from the variables of the specified environment.
-   * @param array $environment An array providing environment variables. Defaults to `$_ENV` if not empty, otherwise `$_SERVER`.
+   * @param array $env An array providing environment variables. Defaults to `$_ENV` if not empty, otherwise `$_SERVER`.
    * @return Configuration The newly created configuration.
    */
-  public static function fromEnvironment(array $environment = []): self {
+  public static function fromEnvironment(array $env = []): self {
     $config = new static();
-    if (!$environment) $environment = $_ENV ?: $_SERVER;
+    if (!$env) $env = $_ENV ?: $_SERVER;
 
     // Standard.
-    $serviceName = $environment['CI_NAME'] ?: '';
+    $serviceName = $env['CI_NAME'] ?: '';
     if (mb_strlen($serviceName)) $config['service_name'] = $serviceName;
 
-    if (isset($environment['CI_BRANCH'])) $config['service_branch'] = $environment['CI_BRANCH'];
-    if (isset($environment['CI_BUILD_NUMBER'])) $config['service_number'] = $environment['CI_BUILD_NUMBER'];
-    if (isset($environment['CI_BUILD_URL'])) $config['service_build_url'] = $environment['CI_BUILD_URL'];
-    if (isset($environment['CI_COMMIT'])) $config['commit_sha'] = $environment['CI_COMMIT'];
-    if (isset($environment['CI_JOB_ID'])) $config['service_job_id'] = $environment['CI_JOB_ID'];
+    if (isset($env['CI_BRANCH'])) $config['service_branch'] = $env['CI_BRANCH'];
+    if (isset($env['CI_BUILD_NUMBER'])) $config['service_number'] = $env['CI_BUILD_NUMBER'];
+    if (isset($env['CI_BUILD_URL'])) $config['service_build_url'] = $env['CI_BUILD_URL'];
+    if (isset($env['CI_COMMIT'])) $config['commit_sha'] = $env['CI_COMMIT'];
+    if (isset($env['CI_JOB_ID'])) $config['service_job_id'] = $env['CI_JOB_ID'];
 
-    if (isset($environment['CI_PULL_REQUEST']) && preg_match('/(\d+)$/', $environment['CI_PULL_REQUEST'], $matches)) {
+    if (isset($env['CI_PULL_REQUEST']) && preg_match('/(\d+)$/', $env['CI_PULL_REQUEST'], $matches)) {
       if (count($matches) >= 2) $config['service_pull_request'] = $matches[1];
     }
 
     // Coveralls.
-    if (isset($environment['COVERALLS_REPO_TOKEN']) || isset($environment['COVERALLS_TOKEN']))
-      $config['repo_token'] = $environment['COVERALLS_REPO_TOKEN'] ?: $environment['COVERALLS_TOKEN'];
+    if (isset($env['COVERALLS_REPO_TOKEN']) || isset($env['COVERALLS_TOKEN']))
+      $config['repo_token'] = $env['COVERALLS_REPO_TOKEN'] ?: $env['COVERALLS_TOKEN'];
 
-    if (isset($environment['COVERALLS_COMMIT_SHA'])) $config['commit_sha'] = $environment['COVERALLS_COMMIT_SHA'];
-    if (isset($environment['COVERALLS_PARALLEL'])) $config['parallel'] = $environment['COVERALLS_PARALLEL'];
-    if (isset($environment['COVERALLS_RUN_AT'])) $config['run_at'] = $environment['COVERALLS_RUN_AT'];
-    if (isset($environment['COVERALLS_SERVICE_BRANCH'])) $config['service_branch'] = $environment['COVERALLS_SERVICE_BRANCH'];
-    if (isset($environment['COVERALLS_SERVICE_JOB_ID'])) $config['service_job_id'] = $environment['COVERALLS_SERVICE_JOB_ID'];
-    if (isset($environment['COVERALLS_SERVICE_NAME'])) $config['service_name'] = $environment['COVERALLS_SERVICE_NAME'];
+    if (isset($env['COVERALLS_COMMIT_SHA'])) $config['commit_sha'] = $env['COVERALLS_COMMIT_SHA'];
+    if (isset($env['COVERALLS_PARALLEL'])) $config['parallel'] = $env['COVERALLS_PARALLEL'];
+    if (isset($env['COVERALLS_RUN_AT'])) $config['run_at'] = $env['COVERALLS_RUN_AT'];
+    if (isset($env['COVERALLS_SERVICE_BRANCH'])) $config['service_branch'] = $env['COVERALLS_SERVICE_BRANCH'];
+    if (isset($env['COVERALLS_SERVICE_JOB_ID'])) $config['service_job_id'] = $env['COVERALLS_SERVICE_JOB_ID'];
+    if (isset($env['COVERALLS_SERVICE_NAME'])) $config['service_name'] = $env['COVERALLS_SERVICE_NAME'];
 
     // Git.
-    if (isset($environment['GIT_AUTHOR_EMAIL'])) $config['git_author_email'] = $environment['GIT_AUTHOR_EMAIL'];
-    if (isset($environment['GIT_AUTHOR_NAME'])) $config['git_author_name'] = $environment['GIT_AUTHOR_NAME'];
-    if (isset($environment['GIT_BRANCH'])) $config['service_branch'] = $environment['GIT_BRANCH'];
-    if (isset($environment['GIT_COMMITTER_EMAIL'])) $config['git_committer_email'] = $environment['GIT_COMMITTER_EMAIL'];
-    if (isset($environment['GIT_COMMITTER_NAME'])) $config['git_committer_name'] = $environment['GIT_COMMITTER_NAME'];
-    if (isset($environment['GIT_ID'])) $config['commit_sha'] = $environment['GIT_ID'];
-    if (isset($environment['GIT_MESSAGE'])) $config['git_message'] = $environment['GIT_MESSAGE'];
+    if (isset($env['GIT_AUTHOR_EMAIL'])) $config['git_author_email'] = $env['GIT_AUTHOR_EMAIL'];
+    if (isset($env['GIT_AUTHOR_NAME'])) $config['git_author_name'] = $env['GIT_AUTHOR_NAME'];
+    if (isset($env['GIT_BRANCH'])) $config['service_branch'] = $env['GIT_BRANCH'];
+    if (isset($env['GIT_COMMITTER_EMAIL'])) $config['git_committer_email'] = $env['GIT_COMMITTER_EMAIL'];
+    if (isset($env['GIT_COMMITTER_NAME'])) $config['git_committer_name'] = $env['GIT_COMMITTER_NAME'];
+    if (isset($env['GIT_ID'])) $config['commit_sha'] = $env['GIT_ID'];
+    if (isset($env['GIT_MESSAGE'])) $config['git_message'] = $env['GIT_MESSAGE'];
 
     // CI services.
     $merge = function($service) use ($config) {
@@ -83,16 +83,16 @@ class Configuration implements \ArrayAccess, \Countable, \IteratorAggregate, \Js
       $config->merge(call_user_func("coveralls\\services\\$service\\getConfiguration"));
     };
 
-    if (isset($environment['TRAVIS'])) $merge('travis_ci');
-    else if (isset($environment['APPVEYOR'])) $merge('appveyor');
-    else if (isset($environment['CIRCLECI'])) $merge('circleci');
+    if (isset($env['TRAVIS'])) $merge('travis_ci');
+    else if (isset($env['APPVEYOR'])) $merge('appveyor');
+    else if (isset($env['CIRCLECI'])) $merge('circleci');
     else if ($serviceName == 'codeship') $merge('codeship');
-    else if (isset($environment['GITLAB_CI'])) $merge('gitlab_ci');
-    else if (isset($environment['JENKINS_URL'])) $merge('jenkins');
-    else if (isset($environment['SEMAPHORE'])) $merge('semaphore');
-    else if (isset($environment['SURF_SHA1'])) $merge('surf');
-    else if (isset($environment['TDDIUM'])) $merge('solano_ci');
-    else if (isset($environment['WERCKER'])) $merge('wercker');
+    else if (isset($env['GITLAB_CI'])) $merge('gitlab_ci');
+    else if (isset($env['JENKINS_URL'])) $merge('jenkins');
+    else if (isset($env['SEMAPHORE'])) $merge('semaphore');
+    else if (isset($env['SURF_SHA1'])) $merge('surf');
+    else if (isset($env['TDDIUM'])) $merge('solano_ci');
+    else if (isset($env['WERCKER'])) $merge('wercker');
 
     return $config;
   }
@@ -117,9 +117,9 @@ class Configuration implements \ArrayAccess, \Countable, \IteratorAggregate, \Js
 
   /**
    * Returns a new iterator that allows iterating the elements of this configuration.
-   * @return \Traversable An iterator for the elements of this configuration.
+   * @return \Iterator An iterator for the elements of this configuration.
    */
-  public function getIterator(): \Traversable {
+  public function getIterator(): \Iterator {
     return new \ArrayIterator($this->params);
   }
 
@@ -134,13 +134,15 @@ class Configuration implements \ArrayAccess, \Countable, \IteratorAggregate, \Js
   /**
    * Loads the default configuration.
    * The default values are read from the `.coveralls.yml` file and the environment variables.
+   * @param string $coverallsFile The path to an optional `.coveralls.yml` file. Defaults to the file found in the current directory.
    * @return Configuration The default configuration.
    */
-  public static function loadDefaults(): self {
-    $defaults = new static();
+  public static function loadDefaults(string $coverallsFile = ''): self {
+    if (!mb_strlen($coverallsFile)) $coverallsFile = getcwd().'/.coveralls.yml';
 
-    if (is_file($path = getcwd().'/.coveralls.yml')) {
-      $config = static::fromYAML(@file_get_contents($path));
+    $defaults = new static();
+    if (is_file($coverallsFile)) {
+      $config = static::fromYAML(@file_get_contents($coverallsFile));
       if ($config) $defaults->merge($config);
     }
 
--- test/ConfigurationTest.php
+++ test/ConfigurationTest.php
@@ -8,8 +8,95 @@ use coveralls\{Configuration};
 /**
  * Tests the features of the `coveralls\Configuration` class.
  */
-/*
 class ConfigurationTest extends \PHPUnit_Framework_TestCase {
 
+  /**
+   * Tests the implementation of the `ArrayAccess` interface.
+   */
+  public function testArrayAccess() {
+    $config = new Configuration();
+    $this->assertFalse($config->offsetExists('foo'));
+    $this->assertNull($config->offsetGet('foo'));
+
+    $config->offsetSet('foo', 'bar');
+    $this->assertTrue($config->offsetExists('foo'));
+    $this->assertEquals('bar', $config->offsetGet('foo'));
+
+    $config->offsetUnset('foo');
+    $this->assertFalse($config->offsetExists('foo'));
+    $this->assertNull($config->offsetGet('foo'));
+  }
+
+  /**
+   * Tests the `Configuration::count()` method.
+   */
+  public function testCount() {
+    $this->assertEquals(0, (new Configuration())->count());
+    $this->assertEquals(2, (new Configuration(['foo' => 'bar', 'bar' => 'baz']))->count());
+  }
+
+  /**
+   * Tests the `Configuration::fromEnvironment()` method.
+   */
+  public function testFromEnvironment() {
+    // TODO
+  }
+
+  /**
+   * Tests the `Configuration::fromYAML()` method.
+   */
+  public function testFromYAML() {
+    // TODO
+  }
+
+  /**
+   * Tests the `Configuration::getIterator()` method.
+   */
+  public function testGetIterator() {
+    $iterator = (new Configuration())->getIterator();
+    $this->assertFalse($iterator->valid());
+
+    $iterator = (new Configuration(['foo' => 'bar', 'bar' => 'baz']))->getIterator();
+    $this->assertTrue($iterator->valid());
+
+    $this->assertEquals('bar', $iterator->current());
+    $iterator->next();
+
+    $this->assertEquals('baz', $iterator->current());
+    $iterator->next();
+
+    $this->assertFalse($iterator->valid());
+  }
+
+  /**
+   * Tests the `SourceFile::jsonSerialize()` method.
+   */
+  public function testJsonSerialize() {
+    $this->assertCount(0, get_object_vars((new Configuration())->jsonSerialize()));
+
+    $map = (new Configuration(['foo' => 'bar', 'bar' => 'baz']))->jsonSerialize();
+    $this->assertCount(2, get_object_vars($map));
+    $this->assertEquals('bar', $map->foo);
+    $this->assertEquals('baz', $map->bar);
+  }
+
+  /**
+   * Tests the `Configuration::loadDefaults()` method.
+   */
+  public function testLoadDefaults() {
+    // TODO
+  }
+
+  /**
+   * Tests the `Configuration::merge()` method.
+   */
+  public function testMerge() {
+    $config = new Configuration();
+    $this->assertCount(0, $config);
+
+    $config->merge(new Configuration(['foo' => 'bar', 'bar' => 'baz']));
+    $this->assertCount(2, $config);
+    $this->assertEquals('bar', $config['foo']);
+    $this->assertEquals('baz', $config['bar']);
+  }
 }
-*/
