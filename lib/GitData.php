c804fcb3f7bab6d740ce99dc91fd6d105ad10f35
Cédric Belin <cedric@belin.io>
2017-01-27 20:46:39 +0100

    Implemented the `GitData` class

--- lib/GitData.php
+++ lib/GitData.php
@@ -10,15 +10,29 @@ namespace coveralls;
 class GitData {
 
   /**
-   * @var \ArrayObject TODO
+   * @var string The branch name.
+   */
+  private $branch;
+
+  /**
+   * @var GitCommit The Git commit.
+   */
+  private $commit;
+
+  /**
+   * @var \ArrayObject The remote repositories.
    */
   private $remotes;
 
   /**
    * Initializes a new instance of the class.
-   * @param array $remotes TODO
+   * @param GitCommit $commit The Git commit.
+   * @param string $branch The branch name.
+   * @param array $remotes The remote repositories.
    */
-  public function __construct(array $remotes = []) {
+  public function __construct(GitCommit $commit = null, string $branch = '', array $remotes = []) {
+    $this->setBranch($branch);
+    $this->setCommit($commit);
     $this->remotes = new \ArrayObject($remotes);
   }
 
@@ -29,5 +43,85 @@ class GitData {
   public function __toString(): string {
     $json = json_encode($this, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
     return static::class." $json";
+  }
+
+  /**
+   * Creates a new remote from the specified JSON map.
+   * @param mixed $map A JSON map representing a branch data.
+   * @return GitData The instance corresponding to the specified JSON map, or `null` if a parsing error occurred.
+   */
+  public static function fromJSON($map) {
+    if (is_array($map)) $map = (object) $map;
+    return !is_object($map) ? null : new static(
+      isset($map->head) ? GitCommit::fromJSON($map->head) : null,
+      isset($map->branch) && is_string($map->branch) ? $map->branch : '',
+      isset($map->remotes) && is_array($map->remotes) ? $map->remotes : []
+    );
+  }
+
+  /**
+   * Gets the branch name.
+   * @return string The branch name.
+   */
+  public function getBranch(): string {
+    return $this->branch;
+  }
+
+  /**
+   * Gets the Git commit.
+   * @return GitCommit The Git commit.
+   */
+  public function getCommit() {
+    return $this->commit;
+  }
+
+  /**
+   * Gets the remote repositories.
+   * @return \ArrayObject The remote repositories.
+   */
+  public function getRemotes(): \ArrayObject {
+    return $this->remotes;
+  }
+
+  /**
+   * Converts this object to a map in JSON format.
+   * @return \stdClass The map in JSON format corresponding to this object.
+   */
+  public function jsonSerialize(): \stdClass {
+    return (object) [
+      'branch' => $this->getBranch(),
+      'head' => $this->getCommit(),
+      'remotes' => $this->getRemotes()->getArrayCopy()
+    ];
+  }
+
+  /**
+   * Sets the branch name.
+   * @param string $value The new name.
+   * @return GitData This instance.
+   */
+  public function setBranch(string $value): self {
+    $this->branch = $value;
+    return $this;
+  }
+
+  /**
+   * Sets the Git commit.
+   * @param GitCommit $value The new commit.
+   * @return GitData This instance.
+   */
+  public function setCommit(GitCommit $value = null): self {
+    $this->commit = $value;
+    return $this;
+  }
+
+  /**
+   * Sets the remote repositories.
+   * @param GitRemote[] $value The new remote repositories.
+   * @return GitData This instance.
+   */
+  public function setRemotes(array $value): self {
+    $this->getRemotes()->exchangeArray($value);
+    return $this;
   }
 }
