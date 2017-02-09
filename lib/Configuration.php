980545dc095ac26d3041286a151182f344714517
Cédric Belin <cedric@belin.io>
2017-02-09 14:00:24 +0100

    Merging the environment variables from $_ENV and $_SERVER

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -41,7 +41,7 @@ class Configuration implements \ArrayAccess, \Countable, \IteratorAggregate, \Js
    */
   public static function fromEnvironment(array $env = null): self {
     $config = new static();
-    if (!is_array($env)) $env = $_ENV ?: $_SERVER;
+    if (!is_array($env)) $env = array_merge($_ENV, $_SERVER);
 
     echo 'Environment:', PHP_EOL;
     print_r($env);
