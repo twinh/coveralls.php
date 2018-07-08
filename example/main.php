62543ab4bd0edc59b11c56e26d2257560848eab2
Cédric Belin <cedric@belin.io>
2018-07-08 12:49:22 +0200

    Added an example file

---
+++ example/main.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+use Coveralls\{Client, ClientException};
+
+/**
+ * Uploads a coverage report.
+ */
+function main(): void {
+  try {
+    $coverage = file_get_contents('/path/to/coverage.report');
+    (new Client)->upload($coverage);
+    echo 'The report was sent successfully.';
+  }
+
+  catch (\Throwable $e) {
+    echo 'An error occurred: ', $e->getMessage();
+    if ($e instanceof ClientException) echo 'From: ', $e->getUri(), PHP_EOL;
+  }
+}
