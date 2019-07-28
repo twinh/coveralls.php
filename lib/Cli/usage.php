6976a1a779b83590605891d59eb55b32bf270569
Cédric Belin <cedric@belin.io>
2019-07-28 19:25:05 +0200

    Code factorization

---
+++ lib/Cli/usage.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+/** @var string The usage information. */
+return $usage = <<<'EOT'
+Send a coverage report to the Coveralls service.
+
+Usage: coveralls [options] <file>
+
+Options:
+-h, --help       Output usage information.
+-v, --version    Output the version number.
+EOT;
