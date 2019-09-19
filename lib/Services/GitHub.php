48520e3dda9c7a7f22d638cae144b688c72ef3a9
Cédric Belin <cedric@belin.io>
2019-09-19 20:24:08 +0200

    Added the build URL to GitHub service

--- lib/Services/GitHub.php
+++ lib/Services/GitHub.php
@@ -4,7 +4,7 @@ namespace Coveralls\Services;
 use Coveralls\{Configuration};
 
 /** Fetches the [GitHub](https://github.com) configuration parameters from the environment. */
-abstract class TravisCI {
+abstract class GitHub {
 
   /**
    * Gets the configuration parameters from the environment.
@@ -12,9 +12,14 @@ abstract class TravisCI {
    * @return Configuration The configuration parameters.
    */
   static function getConfiguration(array $env): Configuration {
+    $commitSha = $env['GITHUB_SHA'];
+    $gitRef = $env['GITHUB_REF'] ?? '';
+    $repository = $env['GITHUB_REPOSITORY'];
+
     return new Configuration([
-      'commit_sha' => $env['GITHUB_SHA'] ?? null,
-      'service_branch' => $env['GITHUB_REF'] ?? null,
+      'commit_sha' => $commitSha ?? null,
+      'service_branch' => mb_substr($gitRef, 0, 11) == 'refs/heads/' ? mb_substr($gitRef, 11) : null,
+      'service_build_url' => isset($commitSha) && isset($repository) ? "https://github.com/$repository/commit/$commitSha/checks" : null,
       'service_name' => 'github'
     ]);
   }
