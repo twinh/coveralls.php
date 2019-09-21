9e3a79fda78699697cd49a24fb46bfa48592a721
Cédric Belin <cedric@belin.io>
2019-09-21 03:29:23 +0200

    Improved the GitHub support

--- lib/Services/GitHub.php
+++ lib/Services/GitHub.php
@@ -13,12 +13,14 @@ abstract class GitHub {
    */
   static function getConfiguration(array $env): Configuration {
     $commitSha = $env['GITHUB_SHA'];
-    $gitRef = $env['GITHUB_REF'] ?? '';
     $repository = $env['GITHUB_REPOSITORY'];
+
+    $gitRef = $env['GITHUB_REF'] ?? '';
+    $gitRegex = '#^refs/\w+/#';
 
     return new Configuration([
       'commit_sha' => $commitSha ?? null,
-      'service_branch' => mb_substr($gitRef, 0, 11) == 'refs/heads/' ? mb_substr($gitRef, 11) : null,
+      'service_branch' => preg_match($gitRegex, $gitRef) ? preg_replace($gitRegex, '', $gitRef) : null,
       'service_build_url' => isset($commitSha) && isset($repository) ? "https://github.com/$repository/commit/$commitSha/checks" : null,
       'service_name' => 'github'
     ]);
